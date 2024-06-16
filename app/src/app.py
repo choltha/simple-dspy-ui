1ecfd612e07fa948f7ea5bd584e5c14fe3544440
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2024-06-16 12:14:21 +0200

    feat: add debugpy

--- app/src/app.py
+++ app/src/app.py
@@ -2,6 +2,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+import debugpy
+
+if os.getenv("DEBUG") == "1":
+    import debugpy
+    debugpy.listen(("localhost", 5678))
+    debugpy.wait_for_client()
+
+
 import gradio as gr
 
 def greet(name):
