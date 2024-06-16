97092d203dd9769ff9b9e30daccecfc4dd875229
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2024-06-16 12:04:30 +0200

    feat: add dotenv

--- app/src/app.py
+++ app/src/app.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import gradio as gr
 
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-# demo.launch()  
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
