e9a9e267be2eb389bbbfce1c7a4ed0752a14a4b3
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2024-06-16 16:29:24 +0200

    enh: improve examples in ui

--- backend/app/simple_dspy_ui/ui.py
+++ backend/app/simple_dspy_ui/ui.py
@@ -17,10 +17,10 @@ class SimpleDSPyUI:
         with gr.Blocks() as gui:
             gr.Markdown("## Simply optimize your prompts!")
             with gr.Row():
-                signature = gr.Textbox(label="A meta description of what your task is. Can be very concise.", placeholder="Prompt -> Flash Fiction")
+                signature = gr.Textbox(label="A meta description of what your task is. Can be very concise.", placeholder="keywords -> Flash Fiction")
             gr.Markdown("A good example is worth 100 words of description.")
             with gr.Row():
-                example_task1_input = gr.Textbox(label="Example input context", placeholder="A short story about a frog who jumps on a log.")
+                example_task1_input = gr.Textbox(label="Example input context", placeholder="frog, jump, log")
                 example_task1_gold_result = gr.Textbox(label="Example golden output (what should the lm generate in the best case)", placeholder="The frog jumps on the log, slips and lands on the ground.")
             optimize_button = gr.Button("Find optimized prompt.")
             gr.Markdown("DSPy uses fewshot learning to optimize your results")
