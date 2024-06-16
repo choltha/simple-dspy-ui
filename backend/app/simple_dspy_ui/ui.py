317bbad01ec50e8a27542be7cb8721713f59fb9b
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2024-06-16 16:26:54 +0200

    feat: use class for ui, add model for live inference

--- backend/app/simple_dspy_ui/ui.py
+++ backend/app/simple_dspy_ui/ui.py
@@ -1,27 +1,36 @@
 import gradio as gr
 
-def optimize_prompt(signature, example_task1_input, example_task1_gold_result):
-    return f"optimized prompt dummy - signature: {signature}, example_task1_input: {example_task1_input}, example_task1_gold_result: {example_task1_gold_result}"
+# from app.dspy_programs.simple_optimizer import optimize_prompt
 
-def test_prompt(optimized_prompt, test_input_context):
-    return f"test prompt dummy - optimized_prompt: {optimized_prompt}, test_input_context: {test_input_context}"
+class SimpleDSPyUI:
+    def __init__(self, target_model, optimizer_model):
+        self.target_model = target_model
+        self.optimizer_model = optimizer_model
 
-with gr.Blocks() as gui:
-    gr.Markdown("## Simply optimize your prompts!")
-    with gr.Row():
-        signature = gr.Textbox(label="A meta description of what your task is. Can be very concise.", placeholder="Prompt -> Flash Fiction")
-    gr.Markdown("A good example is worth 100 words of description.")
-    with gr.Row():
-        example_task1_input = gr.Textbox(label="Example input context", placeholder="A short story about a frog who jumps on a log.")
-        example_task1_gold_result = gr.Textbox(label="Example golden output (what should the lm generate in the best case)", placeholder="The frog jumps on the log, slips and lands on the ground.")
-    optimize_button = gr.Button("Find optimized prompt.")
-    gr.Markdown("DSPy uses fewshot learning to optimize your results")
-    with gr.Row():
-        optimized_prompt = gr.Textbox(label="Optimized prompt with fewshot result", interactive=False)
-    gr.Markdown("Test your optimized prompt")
-    with gr.Row():
-        test_input_context = gr.Textbox(label="Test prompt context", placeholder="A short story about becoming a butterfly.")
-        test_output = gr.Textbox(label="Test prompt result", interactive=False)
-    test_button = gr.Button("Generate Test result")
-    test_button.click(fn=test_prompt, inputs=[optimized_prompt, test_input_context], outputs=test_output)
-    optimize_button.click(fn=optimize_prompt, inputs=[signature, example_task1_input, example_task1_gold_result], outputs=optimized_prompt)
+    def optimize_prompt(self, signature, example_task1_input, example_task1_gold_result):
+        return f"optimized prompt dummy - signature: {signature}, example_task1_input: {example_task1_input}, example_task1_gold_result: {example_task1_gold_result}"
+
+    def test_prompt(self, optimized_prompt, test_input_context):
+        return self.target_model(optimized_prompt + test_input_context)
+    
+    def create_ui(self):
+        with gr.Blocks() as gui:
+            gr.Markdown("## Simply optimize your prompts!")
+            with gr.Row():
+                signature = gr.Textbox(label="A meta description of what your task is. Can be very concise.", placeholder="Prompt -> Flash Fiction")
+            gr.Markdown("A good example is worth 100 words of description.")
+            with gr.Row():
+                example_task1_input = gr.Textbox(label="Example input context", placeholder="A short story about a frog who jumps on a log.")
+                example_task1_gold_result = gr.Textbox(label="Example golden output (what should the lm generate in the best case)", placeholder="The frog jumps on the log, slips and lands on the ground.")
+            optimize_button = gr.Button("Find optimized prompt.")
+            gr.Markdown("DSPy uses fewshot learning to optimize your results")
+            with gr.Row():
+                optimized_prompt = gr.Textbox(label="Optimized prompt with fewshot result", interactive=False)
+            gr.Markdown("Test your optimized prompt")
+            with gr.Row():
+                test_input_context = gr.Textbox(label="Test prompt context", placeholder="A short story about becoming a butterfly.")
+                test_output = gr.Textbox(label="Test prompt result", interactive=False)
+            test_button = gr.Button("Generate Test result")
+            test_button.click(fn=self.test_prompt, inputs=[optimized_prompt, test_input_context], outputs=test_output)
+            optimize_button.click(fn=self.optimize_prompt, inputs=[signature, example_task1_input, example_task1_gold_result], outputs=optimized_prompt)
+        return gui
--- backend/main.py
+++ backend/main.py
@@ -16,15 +16,30 @@ os.environ['DSP_CACHEDIR'] = os.path.join(os.getcwd(), 'cache')
 os.environ['DSP_NOTEBOOK_CACHEDIR'] = os.environ['DSP_CACHEDIR']
 
 import logging
-import os
-
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import gradio as gr
 
-from app.simple_dspy_ui.ui import gui
+# --- build stuff here before moving to modules ---
 
+import dspy
+import os
+
+# @todo move to backends, maybe use dspy dev-branch instead?
+api_key = os.getenv("API_KEY_OPENAI")
+gpt_turbo = dspy.OpenAI(
+    model = 'gpt-3.5-turbo', 
+    api_key = api_key, 
+    model_type='chat',
+    temperature=0.0,
+    max_tokens=1000
+)
+
+# --- end of build area ---
+
+from app.simple_dspy_ui.ui import SimpleDSPyUI
+gui = SimpleDSPyUI(target_model=gpt_turbo, optimizer_model=gpt_turbo).create_ui()
 app = FastAPI(title="Simple DSPy UI")
 
 environment = os.getenv("ENVIRONMENT", "dev")  # Default to 'development' if not set
