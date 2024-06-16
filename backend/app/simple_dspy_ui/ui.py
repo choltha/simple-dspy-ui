3f588e26ca04ceb87f6fa0cde9c751a146c55882
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2024-06-16 13:59:10 +0200

    feat: very basic demo dummy

--- backend/app/simple_dspy_ui/ui.py
+++ backend/app/simple_dspy_ui/ui.py
@@ -1,6 +1,7 @@
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
+def optimize_prompt(signature, example_task1_input, example_task1_gold_result):
+    return f"optimized prompt dummy - signature: {signature}, example_task1_input: {example_task1_input}, example_task1_gold_result: {example_task1_gold_result}"
 
-gradio_interface = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+gradio_interface = gr.Interface(fn=optimize_prompt, inputs=["text", "text", "text"], outputs="text", flagging_options=["good", "bad"])
