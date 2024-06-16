9b0b4d7dfe27db135751865324a3a593542212f9
Christoph Holthaus <christoph.holthaus+git@choltha.net>
2024-06-16 12:58:03 +0200

    wip: app

--- app/src/app.py
+++ app/src/app.py
@@ -2,19 +2,36 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 import debugpy
+import os
 
 if os.getenv("DEBUG") == "1":
     import debugpy
     debugpy.listen(("localhost", 5678))
     debugpy.wait_for_client()
 
-
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
+from app.gradio_ui.ui import gradio_iface
+
+
+app = FastAPI(title="DSPy x FastAPI")
+
+environment = os.getenv("ENVIRONMENT", "dev")  # Default to 'development' if not set
+
+if environment == "dev":
+    logger = logging.getLogger("uvicorn")
+    logger.warning("Running in development mode - allowing CORS for all origins")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+app = gr.mount_gradio_app(app, gradio_iface, path="/gradio")
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
